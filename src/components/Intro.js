bb2c3c862eab262017a889b7822b28f73af06d33
Gunveer Vilkhu <gvilkhu@uwo.ca>
2024-07-30 19:55:04 -0400

    update intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,6 +4,7 @@ import '../App.css';
 function Intro() {
   return (
     <div>
+      <Typography variant="h1">Hello!</Typography>
       <Typography variant="h1">Gunveer Vilkhu</Typography>
       <Typography variant="h2">Software Engineer</Typography>
     </div>
