1eb17222df0f60b4c4e7cca3641ff6c34d5e8b2b
Gunveer Vilkhu <gvilkhu@uwo.ca>
2024-07-31 21:46:40 -0400

    added dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 import { Box } from '@mui/material';
 import Intro from './components/Intro';
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+  const handleSetToggle = (event) => {
+    setToggle(event.target.checked);
+  };
+  const handleTheme = () => (toggle ? 'dark' : 'light');
   return (
-    <Box className="App">
+    <Box className={`App ${handleTheme()}`}>
       <Box className="padding-space">
-        <Intro />
+        <Intro handleSetToggle={handleSetToggle} toggle={toggle} />
       </Box>
     </Box>
   );
