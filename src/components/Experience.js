0d3b5b5f5a6fd54dbced4c2ef980f2be9dff5b0c
Gunveer Vilkhu <gvilkhu@uwo.ca>
2024-08-02 14:57:40 -0400

    chips

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,7 @@
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
 import '../App.css';
 
 function Experience() {
@@ -17,11 +18,31 @@ function Experience() {
         <Grid item xs={0}>
           <Typography variant="h2">Software Engineer Intern @ Mobials</Typography>
           <Typography variant="h4">May '23 - Aug '24</Typography>
+          <Grid
+            container
+            direction="row"
+            spacing={1}
+            alignItems="center"
+            justifyContent="left"
+            className="padding-grid"
+          >
+            { ['PHP', 'Vue.js', 'SQL', 'Docker', 'AWS EC2'].map((technology) => <Grid item xs={0}><Chip label={technology} className="text-align chip" size="small" /></Grid>) }
+          </Grid>
         </Grid>
 
         <Grid item xs={0}>
           <Typography variant="h2">Software Engineer Intern @ Surf</Typography>
           <Typography variant="h4">Jan '22 - Aug '22</Typography>
+          <Grid
+            container
+            direction="row"
+            spacing={1}
+            alignItems="center"
+            justifyContent="left"
+            className="padding-grid"
+          >
+            { ['Node.js', 'React', 'DynamoDB', 'AWS Lambda'].map((technology) => <Grid item xs={0}><Chip label={technology} className="text-align chip" size="small" /></Grid>) }
+          </Grid>
         </Grid>
       </Grid>
     </Box>
