b3661abc570d70ca0557f4684576e997251dd64a
Gunveer Vilkhu <gunveerv123@gmail.com>
2025-06-03 11:28:13 -0400

    fix chip bug

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,7 @@
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Chip from '@mui/material/Chip';
+// import Chip from '@mui/material/Chip';
 import '../App.css';
 
 function Experience() {
